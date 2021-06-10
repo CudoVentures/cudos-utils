1e8abd4a69571b97ee89c7c983d2f9e2560e510c
Kamen Stoykov <k.stoykov@razorlabs.com>
2021-06-10 18:02:02 +0300

    keys

--- project-accounts-converter/app/keys.go
+++ project-accounts-converter/app/keys.go
@@ -1,16 +1,35 @@
 package main
 
 import (
+	"context"
 	"encoding/hex"
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/bech32"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
 )
 
 func main() {
+	client, err := ethclient.Dial("https://mainnet.infura.io/v3/<project id>")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	account := common.HexToAddress("0xb3ccb8FB2533E51893915908CEb85763CeaeA97b")
+	balance, err := client.BalanceAt(context.Background(), account, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(balance) // 25893180161173005034
+}
+
+func a() {
 	fmt.Println(os.Args)
 	if len(os.Args) != 2 {
 		panic(fmt.Sprintf("Expected exactly 1 argument but recieved %d", len(os.Args)-1))
--- project-accounts-converter/go.mod
+++ project-accounts-converter/go.mod
@@ -2,6 +2,9 @@ module cudos.org/accounts-converter
 
 go 1.16
 
-require github.com/cosmos/cosmos-sdk v0.42.5
+require (
+	github.com/cosmos/cosmos-sdk v0.42.5
+	github.com/ethereum/go-ethereum v1.10.3
+)
 
 replace github.com/gogo/protobuf => github.com/regen-network/protobuf v1.3.2-alpha.regen.4
