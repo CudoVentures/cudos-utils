2800e1782f1bc44ea60373f45d3fa95e447cdf04
Kamen Stoykov <k.stoykov@razorlabs.com>
2021-08-25 19:34:59 +0300

    create public testnet wallets

--- project-dev-wallet-manager/src/main.js
+++ project-dev-wallet-manager/src/main.js
@@ -1,8 +1,13 @@
 const { ethers } = require('ethers');
 
+const provider = new ethers.providers.InfuraProvider("rinkeby");
+
 async function main() {
-    const wallet = new ethers.Wallet('');
+    const wallet = new ethers.Wallet('', provider);
+    // const wallet = new ethers.Wallet.createRandom();
     console.log(wallet);
+    const balance = await wallet.getBalance();
+    console.log(ethers.utils.formatEther(balance));
 }
 
 main();
